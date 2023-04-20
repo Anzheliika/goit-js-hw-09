36d05f316e234cb8f5e7e5cac7c178f02d2a2832
Anzheliika <116392828+Anzheliika@users.noreply.github.com>
2023-04-20 16:21:49 +0000

    Deploying to gh-pages from @ Anzheliika/goit-js-hw-09@ef8dd08b47ea7bb85e45bd6ca75fa61406308531 🚀

---
+++ 01-color-switcher.3669f5b6.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(){intervalId=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(intervalId),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.3669f5b6.js.map
